7b13cf1b3a41439840258f7dbe5bdf077fe667bd
majorz <zahari@resin.io>
2017-07-15 10:42:21 +0300

    Trigger a build

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use network_manager::NetworkManager;
 fn main() {
     let service_state = NetworkManager::get_service_state();
 
-    println!("{:?}", service_state);
+    println!("NetworkManager service state: {:?}", service_state);
 }
