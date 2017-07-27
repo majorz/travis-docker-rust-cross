a3dd22acc6bbc1f10bed83b1328ea94e528d26c8
majorz <zahari@resin.io>
2017-07-27 21:46:44 +0300

    stripbin

--- ci/script.sh
+++ ci/script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
 
-#if [ $TARGET != TEST ]; then
-
 docker build -t $TARGET ci/docker/$TARGET
 
 docker run -it --rm -v ${PWD}:/work $TARGET cargo build --release --target=$TARGET
 
-eval $STRIP target/$TARGET/release/$BINARY
+docker run -it --rm -v ${PWD}:/work $TARGET stripbin target/$TARGET/release/$BINARY
