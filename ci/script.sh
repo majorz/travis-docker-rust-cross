ae957013f80f9df1508962a32c741341695d4976
majorz <zahari@resin.io>
2017-07-28 09:45:55 +0300

    protect

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ev
 
+if [ -z "$TARGET" ] || [ -z "$BINARY" ]; then
+	echo "Please set value for TARGET and BINARY"
+	exit 1
+fi
+
 docker build -t $TARGET ci/docker/$TARGET
 
 docker run -it --rm -v ${PWD}:/work $TARGET cargo build --release --target=$TARGET
